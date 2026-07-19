26bebb18e6c700d9cb7cc0e503b5349091e0029e
beneweb55-source <beneweb55@gmail.com>
2026-07-19 14:46:29 +0100

    fix: corriger l'erreur de compilation due au doublon IconeOeil

--- components/icons.tsx
+++ components/icons.tsx
@@ -318,13 +318,6 @@ export const IconeInfo = creerIcone(
   </>
 );
 
-export const IconeOeil = creerIcone(
-  "Oeil",
-  <>
-    <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
-    <circle cx="12" cy="12" r="3" />
-  </>
-);
 
 export const IconeOeilBarre = creerIcone(
   "OeilBarre",
